f59e5738b091439651612e1056464814dc0a51ff
Muhammad Ahmed Saleem <ahmedmughal919530@gmail.com>
2023-08-14 12:21:37 +0500

    done

--- modules/Media/Routes/admin.php
+++
@@ -1,6 +0,0 @@
-<?php
-use Illuminate\Support\Facades\Route;
-Route::get('/','MediaController@index')->name('media.admin.index');
-Route::post('/getLists','MediaController@getLists')->name('media.admin.getLists')->withoutMiddleware('dashboard');
-
-Route::post('/edit_image','MediaController@editImage')->name('media.edit.image');
